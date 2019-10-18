caad2ec4a20af1aa75c02cd06ed4e1f813863257
Philip Igwubor <p.igwubor@gmail.com>
2019-10-19 00:20:08 +0100

    Add files via upload

---
+++ js/details-modal-cakes.php
@@ -0,0 +1,30 @@
+<div class="modal fade details-1" id="details-1" tableindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
+     <div class="modal-dialog modal-lg">
+       <div class="modal-content">
+        <div class="modal-header">
+       <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+       </button>
+       <h4 class="modal-title text-center">Chocolate Cake</h4>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+         <div class="row">
+         <div class="col-sm-6">
+         <div class="center-block">
+          <img src="images/chocolate cake1.jpg" alt="Random Name" class="details img-responsive" />
+      </div>
+      </div>
+         <div class="col-sm-6">
+          <h4>Details</h4>
+          <p>This cake is made from rich flavour chocolate, you can feel the chocolaty tasty from a single bite</p>
+          <hr/>
+          <p>Price: NGN 10,000</p>
+          <p>Brand: Cake</p>
+      </div>
+      </div>
+      </div>
+      </div>
+     </div>
+     <div/>
+</div>
---
+++ js/modalAjax.js
@@ -0,0 +1,26 @@
+
+function showDetails(button){
+  var product_id = button.id;
+
+// AJAX call to get specific product data
+$.ajax({
+  url: "modalAjaxFetchDB.php",
+  method: "GET",
+  data: {"product_id": product_id},
+  success: function(response){
+ //   alert(response);
+// parsing the Jason to javascript object
+var product = JSON.parse(response);
+var path = "catelogue/product_images/";
+var img = product.product_image;
+var imgPath = path + img;
+
+$("#modName").text(product.product_title);
+$("#myimage").attr('src', imgPath);
+$("#myDesc").text(product.product_desc);
+$("#priceG").text(product.product_price);
+$("#product").attr('pid', product.product_id);
+}
+  
+});
+}
